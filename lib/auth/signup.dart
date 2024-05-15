25440beea8b81ac11a9fda22eaa8ecbfa2bd50a9
Bhavishya <bhavishyachaudhary180@gmail.com>
2024-05-15 16:55:23 +0530

    Files Added

---
+++ lib/auth/signup.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class signup extends StatefulWidget {
+  const signup({super.key});
+
+  @override
+  State<signup> createState() => _signupState();
+}
+
+class _signupState extends State<signup> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Welcome to Twitter"),
+    );
+  }
+}
