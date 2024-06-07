5fd34564e709295df13f67094fd12a0e138fe9b9
Bhavishya <bhavishyachaudhary180@gmail.com>
2024-06-08 00:13:52 +0530

    Code changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,16 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:twitter_clone/auth/signup.dart';
+// import 'package:loginuicolors/login.dart';
+// import 'package:loginuicolors/register.dart';
+import 'package:twitter_clone/login.dart';
+import 'package:twitter_clone/register.dart';
 
-import 'firebase_options.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  //const MyApp({super.key});
-
-  // This widget is the root of your application.
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Signup(),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyLogin(),
+    routes: {
+      'register': (context) => MyRegister(),
+      'login': (context) => MyLogin(),
+    },
+  ));
 }
